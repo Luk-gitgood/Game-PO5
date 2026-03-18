bd78590556ec0ec6815a3b4b1cd5c488decd2773
Ingmar <ingmar@MacBook-Air-van-Ingmar.local>
2026-03-18 21:06:03 +0100

    added hotbar to ui

--- code/ui.py
+++ code/ui.py
@@ -45,6 +45,26 @@ class UI:
         self.bg = pygame.image.load(graphics_path / "health_bar.png").convert_alpha()
         self.bg = pygame.transform.scale(self.bg, (PLAYER_HEALTH_BAR_WIDTH, PLAYER_BAR_HEIGHT))
 
+        # Hotbar setup
+        self.hotbar_slots = ['dagger', 'revolver', 'shotgun', 'sniper']
+        self.hotbar_keys = ['2', '3', '4', '5',]
+        
+        # Laad icons
+        graphics_path = BASE_DIR.parent / "graphics" / "weapons"
+        self.weapon_icons = {
+            'dagger': pygame.image.load(graphics_path / "dagger.png").convert_alpha(),
+            'revolver': pygame.image.load(graphics_path / "revolver.png").convert_alpha(),
+            'shotgun': pygame.image.load(graphics_path / "shotgun.png").convert_alpha(),
+            'sniper': pygame.image.load(graphics_path / "sniper.png").convert_alpha(),
+        }
+
+        # Schaal icons
+        for key in self.weapon_icons:
+            self.weapon_icons[key] = pygame.transform.scale(
+                self.weapon_icons[key], (50, 20)
+            )
+
+
     def show_bar(self, current_amount, max_amount, bg_rect, color):
         """
         Tekent een rechthoekige balk gebaseerd op de ratio van huidige naar maximale gezondheid.
@@ -62,12 +82,47 @@ class UI:
 
         pygame.draw.rect(self.display_surface, color, current_rect)
 
+ 
+    def display_hotbar(self, player):
+        """
+        Displayt een hotbar met alle wapens, en highlight het nummer van het wapen
+        
+        Args: 
+            player : player info is nodig voor informatie welk wapen geselecteerd is
+        """
+        screen_width = self.display_surface.get_width()
+        y = self.display_surface.get_height() - 60 #y positie is 60 px van de grond
+
+        spacing = 70 #pixels tussen elk icon
+        total_width = len(self.hotbar_slots) * spacing 
+        start_x = 40
+
+        for index, weapon in enumerate(self.hotbar_slots):
+            x = start_x + index * spacing # x locatie van de hotbar
+
+            # Teken icon
+            icon = self.weapon_icons[weapon]
+            icon_rect = icon.get_rect(center=(x, y))
+            self.display_surface.blit(icon, icon_rect)
+
+            # Highlight alleen het nummer wanneer geselecteerd
+            is_selected = (weapon == player.weapon)
+
+            color = (255, 215, 0) if is_selected else (180, 180, 180) #maakt nummer geel als selected is
+
+            key_text = self.font.render(self.hotbar_keys[index], True, color)
+
+            # zet nummer onder de icons
+            text_rect = key_text.get_rect(center=(x, y + 30))
+            self.display_surface.blit(key_text, text_rect)
+
+
     def display(self, player):
         """
         Update en rendert de volledige UI, inclusief de vloeiende gezondheidsbalk.
 
         Args:
-            player (Player): Het player-object om de gezondheid van te lezen.
+            player (Player): Het player-object om de health van te lezen.
         """
         if self.displayed_health is None:
             self.displayed_health = player.health
@@ -87,3 +142,5 @@ class UI:
 
         #Teken de omlijsting/frame op de voorgrond
         self.display_surface.blit(self.bg, self.player_health_bar_rect.topleft)
+
+        self.display_hotbar(player)
